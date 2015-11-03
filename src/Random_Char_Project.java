0146e26d8a45fec1cf4a923c1b529a4d5eb567df
Jordyn Hartzell <hartzellj@student.swosu.edu>
2015-11-03 13:46:20 -0600

    Added the random method

--- src/Random_Char_Project.java
+++ src/Random_Char_Project.java
@@ -1,14 +1,16 @@
 public class Random_Char_Project {
 	public static void main(String[] args) {
+		
 		// Establish limits
 		int letterA = 65;
 		int letterZ = 90;
+		int letterRange = letterZ - letterA + 1;
 		
 		// Generate random letter
-		int randomLetter = letterA + (int)Math.random() * letterZ;
+		int randomLetter = letterA + (int)Math.random() * letterRange;
 	
 		// Output random letter
-		System.out.print(randomLetter);
+		System.out.print("Your random letter is: " + (char)randomLetter);
 
 	}
 

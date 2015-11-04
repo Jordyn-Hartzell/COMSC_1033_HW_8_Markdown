ffaeaf1e88d06e0901fc1ec86ab0006b7aafdcf3
Jordyn Hartzell <hartzellj@student.swosu.edu>
2015-11-04 14:50:25 -0600

    Added variables and formula for random letter

--- src/Random_Char_Project.java
+++ src/Random_Char_Project.java
@@ -2,13 +2,15 @@ public class Random_Char_Project {
 	public static void main(String[] args) {
 		
 		// Establish limits
+		int startingNumber = 65;
+		int endingNumber   = 90 - 65 + 1;
 		
-		
-		// Generate random letter
-		
+		// Generate random number
+		int randomNumber = (int)(startingNumber + Math.random() * endingNumber);
 	
-		// Output random letter
-		
+		// Output random number as a letter
+		System.out.println(randomNumber + " is your random number.");
+		System.out.print("Therefore, your letter is " + (char)randomNumber + ".");
 
 	}
 
